89d04e0a485324e20d14dc6de59188353e8aba55
gilbert <mbula.gilberto@gmail.com>
2023-05-08 07:13:35 +0100

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -12,10 +12,11 @@ const hosts = [
   '3.21.73.82:4200',
   'd1r4ce9f3jo5h1.cloudfront.net',
   'helios.chida-app.com.s3-website.us-east-2.amazonaws.com',
-  'helios.chida-app.com.s3-website.us-east-2.amazonaws.com',
+  'helios.chida-app.com.s3-website.us-east-2.amazonaws.com:4000',
   'localhost',
   `${HOST_IP}:3000`,
   `${HOST_IP}:3001`,
+  `${HOST_IP}:3002`,
   `${HOST_IP}:4200`,
   `${HOST_IP}:5200`,
   HOST_IP
