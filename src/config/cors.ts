76fa31783238780b190036923bf93091f98784e8
gilbert <mbula.gilberto@gmail.com>
2023-05-08 23:38:41 +0100

    127.0.0.1 cors add

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -8,21 +8,12 @@ const corsOptions = {
 const hosts = [
   'flespi.com',
   'localhost:3000',
-  'localhost:3001',
-  'localhost:3002',
-  '127.0.0.1:4000',
   '127.0.0.1:3000',
-  '127.0.0.1:3001',
-  '127.0.0.1:3002',
-  'localhost:4000',
   'd1r4ce9f3jo5h1.cloudfront.net',
   'helios.chida-app.com.s3-website.us-east-2.amazonaws.com',
   'localhost',
   `${HOST_IP}:3000`,
-  `${HOST_IP}:3001`,
-  `${HOST_IP}:3002`,
   `${HOST_IP}:4200`,
-  `${HOST_IP}:4000`,
   `${HOST_IP}:5200`,
   HOST_IP
 ];
